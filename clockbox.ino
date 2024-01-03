e1bd4bb7c38a93153b362aa61f37fa2c989928b4
IC-Alchemy <153705720+IC-Alchemy@users.noreply.github.com>
2024-01-03 13:05:23 -0700

    Update clockbox.ino
    remove silly code

--- clockbox.ino
+++ clockbox.ino
@@ -61,30 +61,6 @@ void setClockTempo(float tempo) {
   uClock.setTempo(tempo);
 }
 
-void handleSwitchState() {
-  int switchState = readSwitchState(2);
-  if (isSwitchPressed(switchState) && !currentSwitchState) {
-    toggleStartStop();
-    currentSwitchState = true;
-  }
-  
-  if (isSwitchReleased(switchState)) {
-    currentSwitchState = false;
-  }
-}
-
-int readSwitchState(int pin) {
-  return digitalRead(pin);
-}
-
-bool isSwitchPressed(int switchState) {
-  return switchState == HIGH;
-}
-
-bool isSwitchReleased(int switchState) {
-  return switchState == LOW;
-}
-
 void setup() {
 
   Serial.begin(31250);
